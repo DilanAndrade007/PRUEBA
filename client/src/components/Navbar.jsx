1031fdad058a0859f390dc19fe9c08365142a524
Galo Tarapués <105818063+Deadblack216@users.noreply.github.com>
2024-07-03 23:51:59 -0500

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export function Navbar() {
   return (
     <nav className="bg-zinc-700 my-3 flex justify-between py-5 px-10 rounded-lg">
       <h1 className="text-2xl font-bold">
-        <Link to={isAuthenticated ? "/tasks" : "/"}>Task Manager</Link>
+        <Link to={isAuthenticated ? "/tasks" : "/"}>AndesInvest</Link>
       </h1>
       <ul className="flex gap-x-2">
         {isAuthenticated ? (
@@ -29,10 +29,10 @@ export function Navbar() {
         ) : (
           <>
             <li>
-              <ButtonLink to="/login">Login</ButtonLink>
+              <ButtonLink to="/login">Ingresar</ButtonLink>
             </li>
             <li>
-              <ButtonLink to="/register">Register</ButtonLink>
+              <ButtonLink to="/register">Registrarse</ButtonLink>
             </li>
           </>
         )}
