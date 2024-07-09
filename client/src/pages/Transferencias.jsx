d1dbfb47dc190b8cbea0949ebec49ba78253b463
Hernán Sánchez <herez2002@gmail.com>
2024-07-08 23:03:16 -0500

    Update Transferencias.jsx

--- client/src/pages/Transferencias.jsx
+++ client/src/pages/Transferencias.jsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import { useEffect } from "react";
+import { useAuth } from "../context/authContext";
+import { Link, useNavigate } from "react-router-dom";
+import { Card, Message, Button, Input, Label } from "../components/ui";
+import { useForm } from "react-hook-form";
+import { registerSchema } from "../schemas/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const Transferencias = () => {
     return (
