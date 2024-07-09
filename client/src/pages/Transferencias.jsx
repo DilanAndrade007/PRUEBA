f43d28b7d7a7a78b82f68a9ce6f67071d7a2872d
Hernán Sánchez <herez2002@gmail.com>
2024-07-08 21:52:44 -0500

    Create Transferencias.jsx

---
+++ client/src/pages/Transferencias.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import { useAuth } from "../context/authContext";
+import { Link, useNavigate } from "react-router-dom";
+import { Card, Message, Button, Input, Label } from "../components/ui";
+import { useForm } from "react-hook-form";
+import { registerSchema } from "../schemas/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+function Tranferencia() {
+
+}
